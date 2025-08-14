cffa3e96b4c2c4b8a154a77529fc9be2db805516
Dhruva1506 <vidushikumari15@gmail.com>
2025-08-14 16:12:55 +0530

    Create ColorManual.h

---
+++ ColorManual.h
@@ -0,0 +1,16 @@
+#ifndef TELCO_COLORMANUAL_H_
+#define TELCO_COLORMANUAL_H_
+
+#include <iosfwd>
+#include <string>
+
+namespace TelCoColorCoder {
+
+
+std::string FormatReferenceManual();
+
+
+void PrintReferenceManual(std::ostream& os);
+
+}  
+#endif 
