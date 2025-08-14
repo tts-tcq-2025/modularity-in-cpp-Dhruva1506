91424f9cf23548c0a9ebe88dccd22e6cf79b3cd6
Dhruva1506 <vidushikumari15@gmail.com>
2025-08-14 16:11:37 +0530

    Create ColorManual.cpp

---
+++ ColorManual.cpp
@@ -0,0 +1,23 @@
+#include "manual.hpp"
+#include "colorcoder.hpp"
+#include <ostream>
+#include <sstream>
+
+namespace TelCoColorCoder {
+
+std::string FormatReferenceManual() {
+    std::ostringstream out;
+    out << "25-Pair Color Code Reference\n";
+    out << "Pair Number\tColor Pair\n";
+    for (int pairNum = 1; pairNum <= 25; ++pairNum) {
+        ColorPair pair = GetColorFromPairNumber(pairNum);
+        out << pairNum << "\t\t" << pair.ToString() << "\n";
+    }
+    return out.str();
+}
+
+void PrintReferenceManual(std::ostream& os) {
+    os << FormatReferenceManual();
+}
+
+}  
